b648162b6b7e0e26adaf7393f49f913811876757
Danilov11 <danilov@lenta.ru>
2026-05-27 11:20:09 +0300

    Set Railway URL and CORS for GitHub Pages

--- main.py
+++ main.py
@@ -24,7 +24,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://danilov11.github.io",
+        "http://localhost:8000",
+        "http://127.0.0.1:8000",
+    ],
     allow_methods=["*"],
     allow_headers=["*"],
 )
